97ef473f58bc8d61c6b8c1d47cee7bae5dadec88
muptezelinsan <69256318+muptezelinsan@users.noreply.github.com>
2022-05-29 22:01:52 +0300

    Update arch-install.sh

--- arch-install.sh
+++ arch-install.sh
@@ -144,15 +144,9 @@ file="/mnt/home"
 if [ -e $file ]; then
 mount /dev/$homepart /mnt/home
 fi
-
-cp -R /root/arch/ /mnt/home/
-mv /mnt/etc/pacman.d/ /mnt/etc/pacman.d.bak
-cp -R /etc/pacman.d/ /mnt/etc/
-mv /mnt/etc/pacman.conf /mnt/etc/pacman.conf.bak
-cp /etc/pacman.conf /mnt/etc/
-
 mkdir -p /mnt/ikikarinca
-mount -o defaults,noatime,discard=async,ssd /dev/sda5 /mnt/ikikarinda
+mount -o defaults,noatime,discard=async,ssd /dev/sda5 /mnt/ikikarinca
+cp -R /root/arch/ /mnt/home/
 echo ""
 echo "${Bold}${Red}ikikarinca montaji /// !!!! ${Sgr0}"
 sleep 2
@@ -258,6 +252,11 @@ echo ""
 echo "##############################################################################"
 ##############################################################################
 
+mv /mnt/etc/pacman.d/ /mnt/etc/pacman.d.bak
+cp -R /etc/pacman.d/ /mnt/etc/
+mv /mnt/etc/pacman.conf /mnt/etc/pacman.conf.bak
+cp /etc/pacman.conf /mnt/etc/
+
 arch-chroot /mnt /bin/bash -c "pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com"
 arch-chroot /mnt /bin/bash -c "pacman-key --lsign-key FBA220DFC880C036"
 arch-chroot /mnt /bin/bash -c "pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'"
@@ -274,7 +273,8 @@ echo "##########################################################################
 echo "${Bold}${White}-------------------------------------------------${Sgr0}"
 echo "${Bold}${White}-------------------------------------------------${Sgr0}"
 arch-chroot /mnt /bin/bash -c "yay -S ctlos"
-arch-chroot /mnt /bin/bash -c "yay -S --needed - < /home/arch/pkgs.txt"
+arch-chroot /mnt /bin/bash -c "yay -S --needed - < /home/arch/bspwm-packages.txt"
+#arch-chroot /mnt /bin/bash -c "yay -S --needed - < /home/arch/pkgs.txt"
 #arch-chroot /mnt /bin/bash -c "yay -S - < /home/arch/ctlpkg.txt"
 ##############################################################################
 echo "${Bold}${White}-------------------------------------------------${Sgr0}"
@@ -283,10 +283,12 @@ echo "${Bold}${White}-------------------------------------------------${Sgr0}"
 arch-chroot /mnt /bin/bash -c "grub-mkconfig -o /boot/grub/grub.cfg"
 ##############################################################################
 echo "${Bold}${White}-------------------------------------------------${Sgr0}"
-mv /mnt/etc/skel/ /mnt/etc/skel-ctlos/
-mkdir -p /mnt/etc/skel/
+cp -R /mnt/etc/skel/ /mnt/etc/skel-ctlos/
+rm -rf /mnt/etc/skel/
+cp -R /root/arch/etc/skel/ /mnt/etc/
+cp -RT /usr/bin/ /mnt/usr/bin/
 ###arch-chroot /mnt /bin/bash -c "yay -S --needed arcolinux-bspwm-git"
-cp -RT /mnt/etc/skel-ctlos/ /mnt/home/$user/
+cp -RT /mnt/etc/skel/ /mnt/home/$user/
 arch-chroot /mnt /bin/bash -c "chown -R $user:$user /home/$user/"
 echo "${Bold}${White}-------------------------------------------------${Sgr0}"
 ##############################################################################
